cc9d18fb91f543f0a9b749e34ade078982d62dc9
h1zzi43 <avedeneev20@gmail.com>
2025-12-05 14:27:09 +0300

    Задача 1

--- App/Tasks/Task1_CharacterFactory/CharacterTask.cs
+++ App/Tasks/Task1_CharacterFactory/CharacterTask.cs
@@ -1,1 +1,55 @@
+using System;
+using System.Collections.Generic;
+
 namespace App.Tasks.Task1_CharacterFactory;
+
+public interface ICharacter
+{
+    string Name { get; set; }
+    void Attack();
+}
+
+public class Warrior : ICharacter
+{
+    public string Name { get; set; }
+
+    public void Attack()
+    {
+        Console.WriteLine("Воин наносит удар мечом!");
+    }
+}
+
+public class Mage : ICharacter
+{
+    public string Name { get; set; }
+
+    public void Attack()
+    {
+        Console.WriteLine("Маг кастует огненный шар!");
+    }
+}
+
+public class CharacterFactory<T> where T : ICharacter, new()
+{
+    public T Create(string name)
+    {
+        T character = new T();
+        character.Name = name;
+        return character;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var warriorFactory = new CharacterFactory<Warrior>();
+        var mageFactory = new CharacterFactory<Mage>();
+
+        var warrior = warriorFactory.Create("Артур");
+        var mage = mageFactory.Create("Мерлин");
+
+        warrior.Attack();
+        mage.Attack();
+    }
+}
